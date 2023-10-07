2583325d4811efdcbce17f55d14e108706b869e4
JiangKevin <JiangKevin@users.noreply.github.com>
2023-10-07 16:06:31 +0800

    备份一下

--- examples/bezier.cpp
+++ examples/bezier.cpp
@@ -2,38 +2,41 @@
 #include <smk/Shape.hpp>
 #include <smk/Window.hpp>
 
-int main() {
-  // Open a new window.
-  auto window = smk::Window(640, 480, "test");
+int main()
+{
+    // Open a new window.
+    auto window = smk::Window( 640, 480, "test" );
 
-  window.ExecuteMainLoop([&] {
-    window.PoolEvents();
-    window.Clear(smk::Color::RGB(0.1f, 0.1f, 0.1f));
-
-    float y = 0.5f + 0.4f * sin(window.time());
-
-    // Draw a bezier path
-    auto bezier = smk::Shape::Bezier(
+    window.ExecuteMainLoop(
+        [ & ]
         {
-            {10, 10},
-            {630 * y, 10},
-            {10, 480 * y},
-            {630, 480 * y},
-        },
-        50);
+            window.PoolEvents();
+            window.Clear( smk::Color::RGB( 0.1f, 0.1f, 0.1f ) );
 
-    auto bezier_path_foreground = smk::Shape::Path(bezier, 10);
-    auto bezier_path_background = smk::Shape::Path(bezier, 20);
+            float y = 0.5f + 0.4f * sin( window.time() );
 
-    bezier_path_background.SetColor(smk::Color::Black);
-    bezier_path_foreground.SetColor(smk::Color::Yellow);
+            // Draw a bezier path
+            auto bezier = smk::Shape::Bezier(
+                {
+                    { 10, 10 },
+                    { 630 * y, 10 },
+                    { 10, 480 * y },
+                    { 630, 480 * y },
+                },
+                50 );
 
-    window.Draw(bezier_path_background);
-    window.Draw(bezier_path_foreground);
+            auto bezier_path_foreground = smk::Shape::Path( bezier, 10 );
+            auto bezier_path_background = smk::Shape::Path( bezier, 20 );
 
-    window.Display();
-  });
-  return EXIT_SUCCESS;
+            bezier_path_background.SetColor( smk::Color::Black );
+            bezier_path_foreground.SetColor( smk::Color::Yellow );
+
+            window.Draw( bezier_path_background );
+            window.Draw( bezier_path_foreground );
+
+            window.Display();
+        } );
+    return EXIT_SUCCESS;
 }
 
 // Copyright 2019 Arthur Sonzogni. All rights reserved.
--- examples/framebuffer.cpp
+++ examples/framebuffer.cpp
@@ -6,59 +6,62 @@
 #include <smk/Sprite.hpp>
 #include <smk/Window.hpp>
 
-int main() {
-  int dim = 512;
-  int center = dim / 2;
-  auto window = smk::Window(dim, dim, "Framebufer example");
+int main()
+{
+    int  dim    = 512;
+    int  center = dim / 2;
+    auto window = smk::Window( dim, dim, "Framebufer example" );
 
-  // Two framebuffer. Reading one and writing in the other. Swapping both for
-  // every iterations.
-  auto framebuffer_1 = smk::Framebuffer(dim, dim);
-  auto framebuffer_2 = smk::Framebuffer(dim, dim);
+    // Two framebuffer. Reading one and writing in the other. Swapping both for
+    // every iterations.
+    auto framebuffer_1 = smk::Framebuffer( dim, dim );
+    auto framebuffer_2 = smk::Framebuffer( dim, dim );
 
-  // Draw a circle under user's mouse.
-  auto circle = smk::Shape::Circle(dim * 0.15);
+    // Draw a circle under user's mouse.
+    auto circle = smk::Shape::Circle( dim * 0.15 );
 
-  window.ExecuteMainLoop([&] {
-    window.PoolEvents();
+    window.ExecuteMainLoop(
+        [ & ]
+        {
+            window.PoolEvents();
 
-    // Use framebuffer_1 to draw into framebuffer_2 and the window -------------
-    auto sprite = smk::Sprite(framebuffer_1);
-    sprite.SetBlendMode(smk::BlendMode::Add);
+            // Use framebuffer_1 to draw into framebuffer_2 and the window -------------
+            auto sprite = smk::Sprite( framebuffer_1 );
+            sprite.SetBlendMode( smk::BlendMode::Add );
 
-    // ----Draw to the window --------------------------------------------------
-    window.Clear(smk::Color::Black);
-    sprite.SetCenter(center, center);
-    sprite.SetPosition(center, center);
-    window.Draw(sprite);
-    window.Display();
+            // ----Draw to the window --------------------------------------------------
+            window.Clear( smk::Color::Black );
+            sprite.SetCenter( center, center );
+            sprite.SetPosition( center, center );
+            window.Draw( sprite );
+            window.Display();
 
-    // ----Draw into framebuffer2 ----------------------------------------------
-    framebuffer_2.Clear(smk::Color::Black);
-    sprite.SetScale(0.5, 0.5);
-    sprite.SetColor({0.6f, 0.7f, 1.f, 1.f});
+            // ----Draw into framebuffer2 ----------------------------------------------
+            framebuffer_2.Clear( smk::Color::Black );
+            sprite.SetScale( 0.5, 0.5 );
+            sprite.SetColor( { 0.6f, 0.7f, 1.f, 1.f } );
 
-    // Top triangle
-    sprite.SetPosition(center, dim * 0.25);
-    framebuffer_2.Draw(sprite);
+            // Top triangle
+            sprite.SetPosition( center, dim * 0.25 );
+            framebuffer_2.Draw( sprite );
 
-    // Bottom left triangle.
-    sprite.SetPosition(dim * 0.25, dim * 0.75);
-    framebuffer_2.Draw(sprite);
+            // Bottom left triangle.
+            sprite.SetPosition( dim * 0.25, dim * 0.75 );
+            framebuffer_2.Draw( sprite );
 
-    // Bottom right triangle.
-    sprite.SetPosition(dim * 0.75, dim * 0.75);
-    framebuffer_2.Draw(sprite);
+            // Bottom right triangle.
+            sprite.SetPosition( dim * 0.75, dim * 0.75 );
+            framebuffer_2.Draw( sprite );
 
-    // Circle under user's mouse.
-    circle.SetPosition(window.input().cursor());
-    framebuffer_2.Draw(circle);
+            // Circle under user's mouse.
+            circle.SetPosition( window.input().cursor() );
+            framebuffer_2.Draw( circle );
 
-    // -------------------------------------------------------------------------
+            // -------------------------------------------------------------------------
 
-    std::swap(framebuffer_1, framebuffer_2);
-  });
-  return EXIT_SUCCESS;
+            std::swap( framebuffer_1, framebuffer_2 );
+        } );
+    return EXIT_SUCCESS;
 }
 
 // Copyright 2019 Arthur Sonzogni. All rights reserved.
